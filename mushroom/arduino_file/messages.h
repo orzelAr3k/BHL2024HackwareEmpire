2ebaa0b2d8a7a2e0d5e913549ec0ebc2668377d3
Karol <karolpl2000@onet.pl>
2024-04-21 05:30:45 +0200

    TCP messages handle

--- mushroom/arduino_file/messages.h
+++ mushroom/arduino_file/messages.h
@@ -1,7 +1,9 @@
 #include <string>
 
 #define DEVICE_ID "10293"
-
+#define TEMP "temperature"
+#define HUM "humidity"
+#define NOISE "noise"  
 const std::string welcome_msg = "{\"type\":\"id\", \"deviceId\":\"" + std::string(DEVICE_ID) + "\"}";
 
 std::string game_response(std::string &usr_rsp, std::string &game_id); 
