fb60b784dc5c75ed283e659db5be48d131282afd
Karol <karolpl2000@onet.pl>
2024-04-21 11:51:26 +0200

    Final version

--- mushroom/arduino_file/arduino_file.ino
+++ mushroom/arduino_file/arduino_file.ino
@@ -1,5 +1,5 @@
 #include <ESP8266WiFi.h>  
-#include <LiquidCrystal.h> //Dołączenie bilbioteki
+#include <LiquidCrystal.h> 
 #include "messages.h"
 #include <map>
 
@@ -7,13 +7,14 @@
 #define PAIR_END_2 "more"
 #define ID "_id"
 #define GAME "name"
+#define LOBBY "lobby"
 #define MAX_PLAYERS "members"
 #define ACTUAL_PLAYERS "players"
 #define GAME_ID "gameId"
 #define MS_500 500
 #define S_30 (30*1000)
 
-LiquidCrystal lcd(D6, D5, D3, D2, D1, D0); //Informacja o podłączeniu nowego wyświetlacza
+LiquidCrystal lcd(D6, D5, D3, D2, D1, D0); // LCD setting
 
 typedef std::pair<std::string, std::string> key_val_pair;
 typedef std::map<std::string, std::string> json_map;
@@ -24,8 +25,8 @@ enum sensor_data{
   TOO_HIGH = 2
 }; 
 
-const char* ssid     ="LPAWiFi1"; // Tu wpisz nazwę swojego wifi
-const char* password = "LPAWiFi1Password"; // Tu wpisz hasło do swojego wifi
+const char* ssid     ="LPAWiFi1"; // WiFi name
+const char* password = "LPAWiFi1Password"; // WiFI password
 std::string game_id = "";
 unsigned long button_time = 0;  
 unsigned long last_button_time = 0;
@@ -47,6 +48,7 @@ void ICACHE_RAM_ATTR button_interrupt(){
       client.print(rsp.c_str());
     }
     else if (decline == LOW){
+      ack_flag = true;
       std::string usr_rsp = "reject";
       std::string rsp = game_response(usr_rsp, game_id);
       client.print(rsp.c_str());
@@ -58,6 +60,21 @@ void ICACHE_RAM_ATTR button_interrupt(){
     }
     last_button_time = button_time;
   }
+}
+
+void print_lobby(json_map &map){
+  std::string name = map[GAME];
+  std::string max = map[MAX_PLAYERS];
+  std::string act = map[ACTUAL_PLAYERS];
+  std::string players = "Players: " + act + "/" + max;
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(name.c_str()); 
+  lcd.setCursor(15, 0); 
+  lcd.print("W"); 
+  lcd.setCursor(0, 1); 
+  lcd.print(players.c_str()); 
+  delay(1000);
 }
 
 void print_no_connection(){
@@ -98,10 +115,10 @@ void print_game_ready(){
 
 void print_server_sck(){
   lcd.clear(); 
-  lcd.begin(16, 2); //Deklaracja typu
-  lcd.setCursor(5, 0); //Ustawienie kursora
+  lcd.begin(16, 2);
+  lcd.setCursor(5, 0); 
   lcd.print("Answer"); 
-  lcd.setCursor(2, 1); //Ustawienie kursora
+  lcd.setCursor(2, 1); 
   lcd.print("acknowledged");
   delay(2000);
 }
@@ -172,25 +189,26 @@ void print_game(json_map &map){
   std::string act = map[ACTUAL_PLAYERS];
   std::string players = "Players: " + act + "/" + max;
   lcd.clear();
-  lcd.setCursor(0, 0); //Ustawienie kursora
-  lcd.print(name.c_str()); //Wyświetlenie tekstu
-  lcd.setCursor(0, 1); //Ustawienie kursora
-  lcd.print(players.c_str()); //Wyświetlenie tekstu
-  // unsigned long time = millis();
-  // unsigned long time_passed = 0;
-  // ack_flag = false;
-  // while (!ack_flag || time_passed > S_30){
-  //   if (ack_flag){
-  //     Serial.println("DELULU");
-  //     break;
-  //   }
-  //   time_passed = millis() - time;
-  //   delay(100);
-  // }
-  // if (!(!ack_flag && time_passed > S_30)){
-  //   std::string rej = "reject";
-  //   client.print(game_response(rej, game_id).c_str());
-  // }
+  lcd.setCursor(0, 0); 
+  lcd.print(name.c_str()); 
+  lcd.setCursor(0, 1); 
+  lcd.print(players.c_str());
+  unsigned long time = millis();
+  unsigned long time_passed = 0;
+  ack_flag = false;
+  while (!ack_flag && time_passed < S_30){
+    time_passed = millis() - time;
+    delay(100);
+  }
+  if (!(ack_flag && time_passed < S_30)){
+    std::string rej = "reject";
+    client.print(game_response(rej, game_id).c_str());
+    lcd.clear();
+    lcd.setCursor(4, 0);
+    lcd.print("Timeout"); 
+    lcd.setCursor(0, 1);
+    lcd.print("Game rejected"); 
+  }
   delay(2000);
 }
 
@@ -209,7 +227,8 @@ void parse_json(json_map &map, std::string message){
     if (pair.first == PAIR_END_1 && pair.second == PAIR_END_2){
       break;
     }
-    map[pair.first] = pair.second;  
+    map[pair.first] = pair.second;
+    delay(10);  
   }
 }
 
@@ -229,10 +248,7 @@ key_val_pair get_key_val(std::string &message){
   auto end_val = message.find("\"", start_val + 1);
   auto val = message.substr(start_val+1, end_val-start_val-1);
   message = message.erase(0, end_val+1);
-  // if (val.find("\"") != std::string::npos){
-  //   message.erase(0, 1);
-  //   message.erase(message.size() - 1);
-  // }
+
   retVal.first = key;
   retVal.second = val;
   Serial.println("key_value_pair");
@@ -246,21 +262,9 @@ void response_to_server(std::string message){
   Serial.println("Msg original");
   Serial.println((String)(msg));
   message.erase(0, 1);
-  // message.erase(message.size() - 1);
-  // message = message + ",";
   json_map map;
   parse_json(map, message);
 
-  // Serial.println("Map below");
-  // for (const auto& c : map){
-  //   const char* key = c.first.c_str();
-  //   const char* val = c.second.c_str();
-  //   Serial.println((String)(key));
-  //   Serial.println((String)(val));
-  // }
-
-  // bool status = true; 
-  // status = check_id(map);
   save_game_id(map);
   std::string msg_type = map["type"];
   if (msg_type == "game"){
@@ -273,6 +277,8 @@ void response_to_server(std::string message){
     print_game_ready();
   } else if (msg_type == "break"){
     print_break();
+  } else if (msg_type == "lobby"){
+    print_lobby(map);
   }
 }
 
@@ -285,12 +291,11 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(D4), button_interrupt, FALLING);
   attachInterrupt(digitalPinToInterrupt(D7), button_interrupt, FALLING);
   attachInterrupt(digitalPinToInterrupt(D8), button_interrupt, FALLING); 
-  //Wyświetlacz LED
-  lcd.begin(16, 2); //Deklaracja typu
-  lcd.setCursor(0, 0); //Ustawienie kursora
-  lcd.print("Good to see you"); //Wyświetlenie tekstu
-  lcd.setCursor(0, 1); //Ustawienie kursora
-  lcd.print("Have a nice day"); //Wyświetlenie tekstu
+  lcd.begin(16, 2); 
+  lcd.setCursor(0, 0); 
+  lcd.print("Good to see you"); 
+  lcd.setCursor(0, 1); 
+  lcd.print("Have a nice day"); 
   
   // Connect to WiFi network
   Serial.println();
@@ -320,7 +325,6 @@ void loop() {
   if (client.connected()){
     String response = client.readStringUntil('}');
     std::string std_response = std::string(response.c_str());
-    // std_response = R"({"gameId":"12345","name":"Szachy"})";
     if (std_response.size()){
       Serial.println(response);
       response_to_server(std_response);
@@ -332,7 +336,5 @@ void loop() {
     client.print(welcome_msg.c_str());
 
   }
-
-  delay(500);
-  
+  delay(250);
 }
